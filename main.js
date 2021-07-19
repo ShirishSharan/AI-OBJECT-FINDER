ab583462df28a58e39666f521c60d566deb13674
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-07-19 18:17:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-function setup() {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380,380);
+video = "";
+
+function preload(){
+    video = createVideo(video.mp4);
     video.hide();
-  }
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center()
+    
+}
+
+function draw(){
+    image(video,0,0,480,380);
+
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status_detected").innerHTML = "Status: detecting objects";
+}
+
+function modelLoaded(){
+    console.log('Model is Loaded !');
+    status = true;
+    video.loop(1);
+    video.speed(1);
+    video.volume(1);
+}
   
